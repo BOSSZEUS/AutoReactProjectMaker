adf259f55d04ed7d85f5e651c63bd5345e71c2f2
Julian <Van Ost>
2020-05-09 23:06:16 -0700

    Adds batch file to root folder. Run npm i @root to auto-install dependencies and build folder structure

--- build_file_architecture.bat
+++ build_file_architecture.bat
@@ -1,1 +1,1 @@
-npm i concurrently express if-env && cd client && npm i && cd src && mkdir views components routes models utils
+npm i concurrently express if-env && cd client && npm i && cd src && mkdir views components routes models utils 
